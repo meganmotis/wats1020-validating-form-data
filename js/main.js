0ea73123fe8170fec62b346398357c3e49485817
meganmotis <meganmotis@gmail.com>
2016-05-16 12:48:14 -0400

    completed assignment reqs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,68 @@
-/* Custom JS goes here. */
-
-// For this assignment you'll need to do a few things:
-// 1. Create a document ready handler.
-// 2. Define a validation object for use on your page.
-// 3. Connect the validation object to an event handler tied to the submit button.
-
-// Refer to the `index.html` file for the validation rules that must be enforced.
+//on ready handler
+$(document).on('ready', function() {
+	//form validation
+	$('#order-form').validate({
+        submitHandler: function(form) {
+            form.submit();
+        },
+        onfocusout: function(element) {
+         $(element).valid();
+        },
+		
+		rules: {
+			"your-name": {
+				required: true,
+				maxlength: 128,
+				lettersonly: true
+			},
+		
+			"your-address": {
+				required: true
+			},
+			
+			"your-city": {
+				required: true
+			},
+			
+			"your-state": {
+				required: true,
+				maxlength: 2,
+				lettersonly: true
+			},
+			
+			"your-zip": {
+				required: true,
+				maxlength: 5,
+				digits: true
+			},
+			
+			"card-holder-name": {
+				required: true,
+				lettersonly: true,
+				maxlength: 128
+			},
+			
+			"card-number": {
+				required: true,
+				creditcard: true
+			},
+			
+			"expiry-month": {
+				required: true
+			},
+			
+			"expiry-year": {
+				required: true
+			},
+			
+			"cvv": {
+				required: true,
+				maxlength: 3,
+				digits: true
+			},
+		}
+	});
+	
+	// Tooltips
+    $('label span.glyphicon').tooltip();
+});
